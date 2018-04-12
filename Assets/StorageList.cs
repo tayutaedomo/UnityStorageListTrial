a6897c59dbacec26e0d08bd1ee93be7e77420002
tayutaedomo <tayutaedomo@gmail.com>
2018-04-12 22:15:41 +0900

    Add GetDirectories

--- Assets/StorageList.cs
+++ Assets/StorageList.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class StorageList : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		string path = "/Users";
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+		path = "/storage";
+#endif
+
+		string[] fs = System.IO.Directory.GetDirectories (path);
+
+		foreach (string s in fs) {
+			Debug.Log ("StorageList: " + s);
+		}
 	}
 	
 	// Update is called once per frame
