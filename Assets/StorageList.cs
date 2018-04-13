f37cc395fc34785f8ef4691a98e3e4bda1dbaeb2
tayutaedomo <tayutaedomo@gmail.com>
2018-04-13 12:38:55 +0900

    Add match logic

--- Assets/StorageList.cs
+++ Assets/StorageList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Text.RegularExpressions;
 
 public class StorageList : MonoBehaviour {
 
@@ -16,10 +17,21 @@ public class StorageList : MonoBehaviour {
 		string[] fs = System.IO.Directory.GetDirectories (path);
 
 		foreach (string s in fs) {
-			Debug.Log ("StorageList: " + s);
+			Debug.Log ("StorageList: " + s + " " + is_matched(s));
 		}
+
+		Debug.Log ("StorageList: null " + is_matched(null));
+		Debug.Log ("StorageList: /storage/3965-6433 " + is_matched("/storage/3965-6433"));
 	}
-	
+
+	bool is_matched(string path) {
+		if (path == null) {
+			return false;
+		}
+
+		return Regex.IsMatch (path, @"^/storage/[0-9]+-[0-9]+");
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
